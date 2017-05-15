c5072d749893df1889a1e213b598c6b8dd84c5c9
Isyman718 <israelcastle2010@gmail.com>
2017-05-15 01:52:17 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 console.log("Up and Running");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var myCards = ["queen","queen","king","king"]
+
+var cardsInPlay = []
+
+var cardOne = ['queen']
+cardsInPlay.push('cardOne');
+
+	if (cardsInPlay.length === 2)
+		alert("You found a match!");
+	else(cardsInPlay.length !== 2)
+		alert("Sorry, try again.");
+
+console.log("User flipped queen");
+
+var cardTwo = ['King']
+
+cardsInPlay.push('cardTwo');
+console.log("User flipped king");
